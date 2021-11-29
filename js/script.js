67865210b8b3567313a57aa50d728de21cd59860
MuhammadVF <vallenleyss@gmail.com>
2021-11-29 23:06:49 +0700

    contact dan header

--- js/script.js
+++ js/script.js
@@ -26,7 +26,8 @@ function test(){
         "width": activeWidthNewAnimWidth + "px"
       });
     });
-  }
+    
+}
   $(document).ready(function(){
     setTimeout(function(){ test(); });
   });
@@ -37,16 +38,6 @@ function test(){
     setTimeout(function(){ test(); });
   });
 
-  var swiper = new Swiper('.blog-slider', {
-    spaceBetween: 30,
-    effect: 'fade',
-    loop: true,
-    mousewheel: {
-      invert: false,
-    },
-    // autoHeight: true,
-    pagination: {
-      el: '.blog-slider__pagination',
-      clickable: true,
-    }
-  });
+
+
+  
