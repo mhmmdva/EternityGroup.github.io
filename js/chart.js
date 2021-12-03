02c8d71ae01a935b2e9ba8844480e08dc269ea96
Izmil <50907550+izmil@users.noreply.github.com>
2021-12-03 15:40:14 +0700

    update data visual buat covid

---
+++ js/chart.js
@@ -0,0 +1,117 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "https://apicovid19indonesia-v2.vercel.app/api/indonesia/provinsi";
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        var data = JSON.parse(this.responseText);
+        //console.log(data);
+        var provinsi = data.map(function(elem) {
+            return elem.provinsi;
+        });
+        //console.log(all)
+        var kasus = data.map(function(elem) {
+            return elem.kasus;
+        });
+        console.log(kasus);
+        var kematian = data.map(function(elem) {
+            return elem.meninggal;
+        });
+
+        const ctx = document.getElementById('chart-line').getContext('2d');
+        const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+        labels: provinsi,
+        datasets: [{
+            label: 'Kasus Covid',
+            data: kasus,
+            backgroundColor: 'transparent',
+            borderColor: 'grey',
+            borderWidth: 2
+        },
+        {
+            label: 'Kematian',
+            data: kematian,
+            backgroundColor: 'transparent',
+            borderColor: 'red',
+            borderWidth: 2
+        }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+                elements:{
+                    line:{
+                        tension:0
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+
+}
+
+var xmlhttp = new XMLHttpRequest();
+var url = "https://apicovid19indonesia-v2.vercel.app/api/indonesia/provinsi";
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+xmlhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        var data = JSON.parse(this.responseText);
+        //console.log(data);
+        var provinsi = data.map(function(elem) {
+            return elem.provinsi;
+        });
+        //console.log(all)
+        var kasus = data.map(function(elem) {
+            return elem.kasus;
+        });
+        console.log(kasus);
+        var kematian = data.map(function(elem) {
+            return elem.meninggal;
+        });
+
+        const ctx = document.getElementById('chart-bar').getContext('2d');
+        const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: provinsi,
+        datasets: [{
+            label: 'Kasus Covid',
+            data: kasus,
+            backgroundColor: 'grey',
+            borderColor: 'grey',
+            borderWidth: 1
+        },
+        {
+            label: 'Kematian',
+            data: kematian,
+            backgroundColor: 'red',
+            borderColor: 'red',
+            borderWidth: 1
+        }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: true,
+                elements:{
+                    line:{
+                        tension:0
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+
+}
